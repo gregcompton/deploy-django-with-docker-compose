5d9ae1728f3634fb6c03013b1dbacf2da048963b
greg <greg@gregcompton.com>
2023-10-24 15:19:51 -0400

    minor tweaks needed for quality of life when cloning for a template

--- app/app/views.py
+++ app/app/views.py
@@ -5,7 +5,7 @@ def index(request):
     content = (f'<h2>Welcome to gregcompton.com</h2>'
                f'<p>Nothing to see here. I just use this for testing.</p>'
                f'<p>If you would like to talk,'
-               f'<a href="https://calendly.com/gregcompton365/let-s-get-to-know-each-other">schedule a time here</a></p>'
+               f'<a href="https://calendly.com/gregcompton365/let-s-get-to-know-each-other"> schedule a time here.</a></p>'
                f'<p>~Greg</p>')
     return HttpResponse(content)
-    # return render(request, 'app/index.html')
+    # return render(request, 'app/index.html'
